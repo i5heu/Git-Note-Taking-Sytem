81115960bff9ece6e9cc0c92f3b93a53e760f8c2
i5heu <i5heu@i5h.eu>
2022-02-02 14:19:35 +0100

    working Journal plugin

--- src/plugins/Journal.ts
+++ src/plugins/Journal.ts
@@ -1,7 +1,87 @@
 import Config, { Plugin } from "../helper/config";
+import fs from 'fs';
 
 export default class Journal {
+    journalBase: string;
+
     constructor(settings: Plugin["settings"], config: Config) {
-        console.log("Journal constructor", settings);
+        const date = new Date();
+        console.log("Plugin Journal: is running");
+        if(typeof settings.journalPath === "string")
+        this.journalBase = Config.basePath + "/" + settings.journalPath;
+        
+        this.createJournalFolderIfNotExists();
+        this.createTemplateFolderIfNotExists();
+        this.createTemplateFileIfNotExists();
+        this.createYearFolderIfNotExists();
+        this.createNewEntry();
+        this.createNewEntry(true);
+
+        console.log("Plugin Journal: is finished");
+    }
+
+    createJournalFolderIfNotExists() {
+        if (!fs.existsSync(this.journalBase)) {
+            fs.mkdirSync(this.journalBase);
+        }
+    }
+
+    // create folder of current year if not exists
+    createYearFolderIfNotExists() {
+        const year = new Date().getFullYear();
+
+        const yearFolder = this.journalBase + "/" + year;
+        if (!fs.existsSync(yearFolder)) {
+            fs.mkdirSync(yearFolder);
+        }
+    }
+
+    // create template folder if not exists
+    createTemplateFolderIfNotExists() {
+        const templateFolder = this.journalBase + "/template";
+        if (!fs.existsSync(templateFolder)) {
+            fs.mkdirSync(templateFolder);
+        }
+    }
+
+    // create template file if not exists
+    createTemplateFileIfNotExists() {
+        let templateFile: string;
+
+        // check if the current weekday file exists if not, create a empty file
+        if(!fs.existsSync(this.journalBase + `/template/${this.weekday}.md`)) {
+            templateFile = "";
+            // write template file
+            fs.writeFileSync(this.journalBase + `/template/${this.weekday}.md`, templateFile);
+        }
+    }
+
+    // copy current weekday template file , with the current date as filename, into the current year folder
+    createNewEntry(nextDay = false) {
+        const date = new Date();
+        
+        let filename: string;
+        if (nextDay) {
+            filename = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}.md`;
+        } else {
+            const dateTomorrow = new Date();
+            dateTomorrow.setDate(date.getDate() + 1);
+            filename = `${dateTomorrow.getFullYear()}-${dateTomorrow.getMonth()}-${dateTomorrow.getDate()}.md`;
+        }
+
+        const filepath = this.journalBase + "/" + date.getFullYear() + "/" + filename;
+
+        fs.writeFileSync(filepath, this.templateFileContentOfCurrentWeekday);
+    }
+
+    // get template file content for the current weekday
+    get templateFileContentOfCurrentWeekday() {
+        const templateFile = fs.readFileSync(this.journalBase + `/template/${this.weekday}.md`, 'utf8');
+        return templateFile;
+    }
+
+    get weekday(){
+        const date = new Date();
+        return date.toLocaleDateString("en-gb", { weekday: "long" });
     }
 }
