85eb83d3c5f7264ae790993aad05c7844a267ee4
i5heu <i5heu@i5h.eu>
2022-04-04 20:24:44 +0200

    basics for Todo Plugin

--- src/helper/fileHelper.ts
+++ src/helper/fileHelper.ts
@@ -24,4 +24,47 @@ export default class FileHelper {
     }
     return string;
   }
+
+  static async appendToFile(path: string, content: string) {
+    try {
+      await fsPromises.appendFile(path, content);
+    } catch (err) {
+      console.error("Error occurred while appending to file!", err);
+    }
+  }
+
+  // will return the header body and optionally the footer
+  static sectionContent(fileText: string) {
+    const lineByLine = fileText.split("\n");
+    const bodyTagLineNumber = lineByLine.findIndex(line => line.indexOf("--- body ---") > -1);
+    const footerTagLineNumber = lineByLine.findIndex(line => line.indexOf("--- footer ---") > -1);
+
+    let header: string[] | undefined;
+    let body: string[] | undefined;
+    let footer: string[] | undefined;
+
+    if (bodyTagLineNumber > -1) {
+      header = lineByLine.slice(0, bodyTagLineNumber);
+    }
+
+    if (bodyTagLineNumber > -1) {
+      if (footerTagLineNumber > -1) {
+        body = lineByLine.slice(bodyTagLineNumber + 1, footerTagLineNumber);
+      } else {
+        body = lineByLine.slice(bodyTagLineNumber + 1, lineByLine.length - 1);
+      }
+    } else if (footerTagLineNumber > -1) {
+      body = lineByLine.slice(0, lineByLine.length - 1);
+    }
+
+    if (footerTagLineNumber > -1) {
+      footer = lineByLine.slice(footerTagLineNumber + 1, lineByLine.length - 1);
+    }
+
+    return {
+      header: header ? header.join("\n") : undefined,
+      body: body ? body.join("\n") : undefined,
+      footer: footer ? footer.join("\n") : undefined
+    }
+  }
 }
---
+++ src/plugins/Todo.ts
@@ -0,0 +1,140 @@
+import Config, { Plugin } from "../config";
+import fs from 'fs';
+import FileHelper from "../helper/fileHelper";
+
+export interface TodoItem {
+    id: string;
+    dependencies: string[] | undefined;
+    repeat: string | undefined;
+    priority: number;
+    type: string;
+    title: string;
+    text: string;
+    completed: boolean;
+    createdAt: Date;
+    beginsAt: Date | undefined;
+    endsAt: Date | undefined;
+    tags: string[] | undefined;
+    durationInMinutes: number | undefined;
+}
+
+export default class ToDo {
+    toDos: TodoItem[] = [];
+    todoBase: string;
+
+    constructor(settings: Plugin["settings"], config: Config) {
+        this.todoBase = Config.basePath + "/" + settings.todoPath;
+    }
+
+    async run() {
+        console.log("Plugin ToDo: is running");
+
+        await this.createToDoFolderIfNotExists();
+        await this.fillTodoArray();
+        const newTodos = await this.searchNewToDos();
+        // await this.addNewToDos(newTodos);
+
+
+        console.log("Plugin ToDo: is finished");
+    }
+
+    createToDoFolderIfNotExists() {
+        if (!fs.existsSync(this.todoBase)) {
+            fs.mkdirSync(this.todoBase);
+        }
+    }
+
+    async fillTodoArray() {
+        this.toDos = [];
+
+        // read from file system
+        FileHelper.getFileListInFolder(this.todoBase).then(async files => {
+            files.forEach(async file => {
+
+                const todoText = await FileHelper.getContentOfFile(this.todoBase + "/" + file);
+                const todo = ParserTodo.parse(todoText);
+                this.toDos.push(todo);
+            });
+        });
+    }
+
+    async searchNewToDos() {
+        const newTodos: TodoItem[] = [];
+    }
+
+    add(todo: TodoItem) {
+        this.toDos.push(todo);
+
+        // add to file system
+    }
+
+    findById(id: string) {
+        return this.toDos.find(todo => todo.id === id);
+    }
+
+    setById(id: string, todo: TodoItem) {
+        const index = this.toDos.findIndex(t => t.id === id);
+        this.toDos[index] = todo;
+    }
+}
+
+class ParserTodo {
+    static parse(todoText: string): TodoItem {
+        const sections = FileHelper.sectionContent(todoText);
+
+        const todo: TodoItem = {
+            id: this.getStringFromSection(sections.header, "id"),
+            dependencies: this.getArrayFromSection(sections.header, "dependencies"),
+            repeat: this.getStringFromSection(sections.header, "repeat"),
+            priority: this.getIntFromSection(sections.header, "priority"),
+            type: this.getStringFromSection(sections.header, "type"),
+            title: this.getStringFromSection(sections.header, "title"),
+            text: sections.body,
+            completed: this.getBooleanFromSection(sections.header, "completed"),
+            createdAt: this.getDateFromSection(sections.header, "createdAt"),
+            beginsAt: this.getDateFromSection(sections.header, "beginsAt"),
+            endsAt: this.getDateFromSection(sections.header, "endsAt"),
+            tags: this.getArrayFromSection(sections.header, "tags"),
+            durationInMinutes: this.getIntFromSection(sections.header, "durationInMinutes")
+        };
+
+        return todo;
+    }
+
+    static getStringFromSection(header: string, key: string): string | undefined {
+        const keyLine = header.split("\n").find(line => line.indexOf(key) > -1);
+        if (!keyLine) return;
+
+        const keyValue = keyLine.split(":")[1].trim();
+        return keyValue;
+    }
+    static getIntFromSection(header: string, key: string): number | undefined {
+        const keyLine = header.split("\n").find(line => line.indexOf(key) > -1);
+        if (!keyLine) return;
+
+        const keyValue = keyLine.split(":")[1].trim();
+        return parseInt(keyValue);
+    }
+
+    static getArrayFromSection(header: string, key: string): string[] | undefined {
+        const keyLine = header.split("\n").find(line => line.indexOf(key) > -1);
+        if (!keyLine) return;
+        const keyValueArray = keyLine.split(":")[1].trim().split(",");
+        return keyValueArray;
+    }
+
+    static getDateFromSection(header: string, key: string): Date | undefined {
+        const keyLine = header.split("\n").find(line => line.indexOf(key) > -1);
+        if (!keyLine) return;
+
+        const keyValue = keyLine.split(":")[1].trim();
+        return new Date(keyValue);
+    }
+
+    static getBooleanFromSection(header: string, key: string): boolean | undefined {
+        const keyLine = header.split("\n").find(line => line.indexOf(key) > -1);
+        if (!keyLine) return;
+
+        const keyValue = keyLine.split(":")[1].trim();
+    }
+}
