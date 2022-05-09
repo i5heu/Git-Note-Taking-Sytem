175237e4917915c7a5a99cd15bb2192a8307e1f8
Mia <mia@fireballlabs.com>
2022-05-09 19:17:57 +0200

    add git pull

--- src/main.ts
+++ src/main.ts
@@ -19,43 +19,48 @@ async function init() {
     const git = new GitManager(conf, lock, pluginManager);
     await git.initialPullOrClone();
     conf.createDefaultConfigIfNotExists();
-    
+
     pluginManager.runPluginsOverFiles();
     pluginManager.schedulePluginRuns();
-    
+
     git.pullInterval();
     lock.emptyQueueFunction = git.commitAndPush.bind(git);
     server(conf, git);
 }
 
-function server(conf, git) {
+function server(conf, git: GitManager) {
     // Compile the Pug templates
     const pug = new Pug();
     // init express
     const app = express();
-    
+
     let loginAttempt: string[number] | undefined[] = [];
-    
+
     // app.get("/", async (req, res) => {
     //     if (!PreRequest.userSpace(req, res)) return;
-    
-    
+
+
     //     const tree = new Tree(git.options.baseDir);
     //     const filesClean = await tree.getFileTree();;
-    
+
     //     renderSend(res, pug.home, {
     //         name: 'Timothy',
     //         files: JSON.stringify(filesClean, null, 2)
     //     });
     // });
-    
+
     // app.get("/login", (req, res) => {
     //     if (!PreRequest.loginAttempts(req, res, loginAttempt)) return;
-    
+
     //     renderSend(res, pug.login, {});
     // });
 
     app.get("/health", (req, res) => {
+        res.send('OK');
+    });
+
+    app.get("/pull", (req, res) => {
+        git.pull();
         res.send('OK');
     });
 
@@ -64,7 +69,7 @@ function server(conf, git) {
     app.listen(conf.port, () => {
         console.log(`server started at http://localhost:${conf.port}`);
     });
-    
+
     // empty the loginAttempt array every 3 minutes
     // TODO: maybe find a better place to put this
     setInterval(() => {
